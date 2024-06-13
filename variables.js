091c4a2cc94d9245537cfc179fd68749ad1e1c86
sainithin2904 <138886334+sainithin2904@users.noreply.github.com>
2024-06-13 10:03:34 +0530

    Update variables.js

--- variables.js
+++ variables.js
@@ -7,3 +7,12 @@ func()
 console.log(x) //x = 12 shows var x is accessible inside the functions as well
 x= 55
 console.log(x) //x = 55 shows var x is reasigned
+
+const c= 5;
+const b = 400;
+console.log(b);
+
+const c = "12";
+console.log(c);
+// c = "new"
+// console.log(c) will show error
