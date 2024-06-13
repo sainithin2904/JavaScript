65bc9cfc2313751a6c8bb6624677c7db9a870a16
sainithin2904 <138886334+sainithin2904@users.noreply.github.com>
2024-06-13 10:01:56 +0530

    var declaration

--- variables.js
+++ variables.js
@@ -1,1 +1,8 @@
 var x = 10;
+
+function func(){
+  return x +=2;
+}
+func()
+console.log(x) //x = 12 shows var x is accessible inside the functions as well
+console.log(x) //x = 55 shows var x is reasigned
